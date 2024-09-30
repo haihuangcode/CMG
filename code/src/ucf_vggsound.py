b412623d2826270d6c36ce384f46e9f43d4e0200
haihuangcode <132985739+haihuangcode@users.noreply.github.com>
2024-09-30 17:06:52 +0800

    Update ucf_vggsound.py
    Remove the unnecessary encoder parameters from the optimizer

--- code/src/ucf_vggsound.py
+++ code/src/ucf_vggsound.py
@@ -142,7 +142,7 @@ def main():
     '''optimizer setting'''
     Encoder.to(device)
     Decoder.to(device)
-    optimizer = torch.optim.Adam(chain(Encoder.parameters(), Decoder.parameters()), lr=args.lr)
+    optimizer = torch.optim.Adam(Decoder.parameters(), lr=args.lr)
 
     scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30], gamma=0.5)
     
